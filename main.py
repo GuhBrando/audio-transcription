6b556f3494f5989d84e2dc3b8327af3f718b87e8
gustavo@gf2.com.br <gustavo@gf2.com.br>
2022-12-10 05:16:45 +0000

    More files

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ STORAGE_ACCOUNT = "6c4d2a82-5766-48b1-a881-a901d58143a8"
 
 # COMMAND ----------
 
-# MAGIC %sh%sh
-# MAGIC ls /local_disk0/.ephemeral_nfs/envs/pythonEnv-4cb87391-5a6d-4cfa-962c-cf2f8ad2fa0d/bin/python
-
-# COMMAND ----------
-
 dbutils.fs.put("/databricks/ffmpeg_install.sh", """ 
  
 #! /bin/bash
@@ -31,7 +26,22 @@ apt-get install -y ffmpeg
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC cp ffmpeg-2022-12-08-git-9ca139b2aa-full_build.7z /local_disk0/.ephemeral_nfs/envs/pythonEnv-4cb87391-5a6d-4cfa-962c-cf2f8ad2fa0d/bin/python/Scripts
+# MAGIC apt-get install libav-tools libavcodec-extra
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC pip install --upgrade pip setuptools wheel
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC sudo apt-get -y install portaudio19-dev
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC sudo apt-get -y install ffmpeg
 
 # COMMAND ----------
 
@@ -45,32 +55,60 @@ apt-get install -y ffmpeg
 
 # COMMAND ----------
 
-import speech_recognition as sr
-import os
-from pydub import AudioSegment
-
-AUDIO_FILE = "file.wav"
-r = sr.Recognizer()
-
-with sr.AudioFile(AUDIO_FILE) as source:
-    audio = r.record(source)
-    print("Transcript: " + r.recognize_google(audio,language="en-US"))
-
-# COMMAND ----------
-
 # MAGIC %sh
-# MAGIC sudo apt-get -y install ffmpeg
-
-# COMMAND ----------
-
-from pydub import AudioSegment
-sound = AudioSegment.from_mp3("TGA 2022 Genshin Impact Entry Video _ Genshin Impact-8BSYycuH-q8.mp3")
-sound.export("file.wav", format="wav")
+# MAGIC pip install pipwin
 
 # COMMAND ----------
 
 # MAGIC %sh
 # MAGIC pip install youtube_dl
+
+# COMMAND ----------
+
+import speech_recognition as sr
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+import numpy as np
+
+sound_file = AudioSegment.from_mp3("My Childhood Obsession with Animals-6E94j0Goo8A.mp3")
+sound_file_Value = np.array(sound_file.get_array_of_samples())
+# milliseconds in the sound track
+ranges = [(30000,100000)] 
+
+for x, y in ranges:
+    new_file=sound_file_Value[x : y]
+    song = AudioSegment(new_file.tobytes(), frame_rate=sound_file.frame_rate,sample_width=sound_file.sample_width,channels=1)
+    song.export(str(x) + "-" + str(y) +".mp3", format="mp3")
+
+# COMMAND ----------
+
+import speech_recognition as sr
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+import numpy as np
+filename = 'myfile.wav'
+
+AUDIO_FILE = "ytbvideo.wav"
+r = sr.Recognizer()
+    
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)
+    text = r.recognize_google(audio, language = 'en-IN', show_all = True)
+    print(text)
+    print("Transcript: " + r.recognize_google(audio, language = 'en-IN', show_all = True))
+
+# COMMAND ----------
+
+from pydub import AudioSegment
+sound = AudioSegment.from_mp3("30000-100000.mp3")
+sound.export("ytbvideo.wav", format="wav")
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC sudo apt-get -y install pulseaudio-equalizer
 
 # COMMAND ----------
 
@@ -81,13 +119,13 @@ audio_downloader = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'wav',
+        'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
 }
 
 with youtube_dl.YoutubeDL(audio_downloader) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=8BSYycuH-q8&ab_channel=GenshinImpact'])
+    ydl.download(['https://www.youtube.com/watch?v=6E94j0Goo8A&ab_channel=JaidenAnimations'])
 
 
 try:
@@ -100,12 +138,6 @@ except Exception:
 
 # COMMAND ----------
 
-# MAGIC %sh
-# MAGIC 
-# MAGIC ffmpeg 
-
-# COMMAND ----------
-
 spark.conf.set(
   f"fs.azure.account.key.{STORAGE_ACCOUNT}.dfs.core.windows.net",
   ""
@@ -113,16 +145,12 @@ spark.conf.set(
 
 # COMMAND ----------
 
-# MAGIC %sh
-# MAGIC pwd
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
 target_folder_path = 'abfss://audiotranscriptor@guhbrandaohotmail.dfs.core.windows.net/audiotranscriptor'
 
 #write as parquet data
 df_covid.write.format("parquet").save(target_folder_path)
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC pkill -f apt
